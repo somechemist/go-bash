7382a6328fbefe40509210a5401458cdc537044a
Justin Powell <somerandomchemist@gmail.com>
2022-05-31 22:41:01 -0500

    first go at go using the book

---
+++ echo/echo.go
@@ -0,0 +1,17 @@
+//This is go based version of echo from the book: "The Go programming Language" by Donovan and Kernighan
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var s, sep string
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
+}
